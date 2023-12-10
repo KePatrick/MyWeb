949a00ef2a0b9c26dcebd26d54758aa722bc687e
KePatrick <147793934+KePatrick@users.noreply.github.com>
2023-12-10 21:25:10 +0800

    Add files via upload

---
+++ MyWeb/src/main/java/tw/patrick/utils/GetHtml.java
@@ -0,0 +1,87 @@
+package tw.patrick.utils;
+
+public class GetHtml {
+	public static String block0Html() {
+		return "<div class=\"container\" style=\"background-color: blanchedalmond; height: 100%;\">\r\n"
+				+ "1 of 1\r\n"
+				+ "</div>";
+	}
+	
+	
+	public static String block1Html() {
+		return "<div class=\"container\" style=\"background-color: blanchedalmond; height: 100%;\">\r\n"
+				+ "\r\n"
+				+ "                <div class=\"row\" style=\"height: 100%;\">\r\n"
+				+ "                    <div class=\"col\" style=\"background-color: aqua;\">\r\n"
+				+ "                        1 of 2\r\n"
+				+ "                    </div>\r\n"
+				+ "                    <div class=\"col\" style=\"background-color: aquamarine ;\">\r\n"
+				+ "                        2 of 2\r\n"
+				+ "                    </div>\r\n"
+				+ "                </div>\r\n"
+				+ "                \r\n"
+				+ "            </div>";
+	}
+	
+	
+	
+ 	public static String block2Html() {
+		return "<div class=\"container\" style=\"background-color: blanchedalmond; height: 100%;\">\r\n"
+				+ "\r\n"
+				+ "                <div class=\"row\" style=\"height: 50%;\">\r\n"
+				+ "                    <div class=\"col\" style=\"background-color: aqua;\">\r\n"
+				+ "                        1 of 2\r\n"
+				+ "                    </div>\r\n"
+				+ "                    <div class=\"col\" style=\"background-color: aquamarine ;\">\r\n"
+				+ "                        2 of 2\r\n"
+				+ "                    </div>\r\n"
+				+ "                </div>\r\n"
+				+ "                <div class=\"row\"style=\"height: 50%;\">\r\n"
+				+ "                    <div class=\"col\" style=\"background-color: aquamarine ;\">\r\n"
+				+ "                        1 of 2\r\n"
+				+ "                    </div>\r\n"
+				+ "                    <div class=\"col\" style=\"background-color: aqua;\">\r\n"
+				+ "                        2 of 2\r\n"
+				+ "                    </div>\r\n"
+				+ "                </div>\r\n"
+				+ "            </div>\r\n"
+				+ "        </div>";
+	}
+
+
+
+	public static String block3Html() {
+	return "<div class=\"container\" style=\"background-color: blanchedalmond; height: 100%;\">\r\n"
+			+ "\r\n"
+			+ "                <div class=\"row\" style=\"height: 50%;\">\r\n"
+			+ "                    <div class=\"col\" style=\"background-color: aqua;\">\r\n"
+			+ "                        1 of 2\r\n"
+			+ "                    </div>\r\n"
+			+ "                    <div class=\"col\" style=\"background-color: aquamarine ;\">\r\n"
+			+ "                        2 of 2\r\n"
+			+ "                    </div>\r\n"
+			+ "                    <div class=\"col\" style=\"background-color: rgb(136, 255, 0);\">\r\n"
+			+ "                        3 of 3\r\n"
+			+ "                    </div>\r\n"
+			+ "                </div>\r\n"
+			+ "                <div class=\"row\"style=\"height: 50%;\">\r\n"
+			+ "                    <div class=\"col\" style=\"background-color: rgb(136, 255, 0);\">\r\n"
+			+ "                        1 of 2\r\n"
+			+ "                    </div>\r\n"
+			+ "                    <div class=\"col\" style=\"background-color: aqua;\">\r\n"
+			+ "                        2 of 2\r\n"
+			+ "                    </div>\r\n"
+			+ "                    <div class=\"col\" style=\"background-color:  aquamarine;\">\r\n"
+			+ "                        3 of 3\r\n"
+			+ "                    </div>\r\n"
+			+ "                </div>\r\n"
+			+ "            </div>";
+}
+
+
+
+
+
+
+
+}
---
+++ MyWeb/src/main/webapp/script.js
@@ -0,0 +1,5 @@
+
+function btn1(){
+    console.log("abcd")
+    document.getElementById("view").innerHTML="<div class='block1'>block1</div><div class='block1'>block2</div>";
+}
